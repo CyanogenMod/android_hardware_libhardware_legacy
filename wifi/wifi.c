b73f4263bf277d6fa3ea2a59a87a4d7e104542d5
Michaël Burtin <bukington@gmail.com>
2011-11-27 15:41:34 +0100

    wifi: move AP driver configuration in board flags
    Add board flags WIFI_AP_DRIVER_MODULE_PATH, WIFI_AP_DRIVER_MODULE_ARG,
    WIFI_AP_DRIVER_MODULE_NAME and WIFI_AP_FIRMWARE_LOADER to allow AP firmware
    customization at device level.
    
    Change-Id: Ia80921e14dc101b3977aa6e37a8a41231e9aa29a

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -59,12 +59,24 @@ static char iface[PROPERTY_VALUE_MAX];
 #define WIFI_DRIVER_MODULE_ARG          ""
 #endif
 #ifndef WIFI_FIRMWARE_LOADER
-#define WIFI_FIRMWARE_LOADER		""
+#define WIFI_FIRMWARE_LOADER            ""
 #endif
 #ifndef WIFI_PRE_LOADER
-#define WIFI_PRE_LOADER		""
+#define WIFI_PRE_LOADER                 ""
 #endif
-#define WIFI_TEST_INTERFACE		"sta"
+#ifndef WIFI_AP_DRIVER_MODULE_NAME
+#define WIFI_AP_DRIVER_MODULE_NAME      "tiap_drv"
+#endif
+#ifndef WIFI_AP_DRIVER_MODULE_PATH
+#define WIFI_AP_DRIVER_MODULE_PATH      "/system/lib/modules/tiap_drv.ko"
+#endif
+#ifndef WIFI_AP_DRIVER_MODULE_ARG
+#define WIFI_AP_DRIVER_MODULE_ARG       ""
+#endif
+#ifndef WIFI_AP_FIRMWARE_LOADER
+#define WIFI_AP_FIRMWARE_LOADER         "wlan_ap_loader"
+#endif
+#define WIFI_TEST_INTERFACE             "sta"
 
 #define WIFI_DRIVER_LOADER_DELAY	1000000
 
@@ -82,11 +94,11 @@ static const char SUPP_CONFIG_FILE[]    = "/data/misc/wifi/wpa_supplicant.conf";
 static const char MODULE_FILE[]         = "/proc/modules";
 static const char PRELOADER[]           = WIFI_PRE_LOADER;
 
-static const char AP_DRIVER_MODULE_NAME[]  = "tiap_drv";
-static const char AP_DRIVER_MODULE_TAG[]   = "tiap_drv" " ";
-static const char AP_DRIVER_MODULE_PATH[]  = "/system/lib/modules/tiap_drv.ko";
-static const char AP_DRIVER_MODULE_ARG[]   = "";
-static const char AP_FIRMWARE_LOADER[]     = "wlan_ap_loader";
+static const char AP_DRIVER_MODULE_NAME[]  = WIFI_AP_DRIVER_MODULE_NAME;
+static const char AP_DRIVER_MODULE_TAG[]   = WIFI_AP_DRIVER_MODULE_NAME " ";
+static const char AP_DRIVER_MODULE_PATH[]  = WIFI_AP_DRIVER_MODULE_PATH;
+static const char AP_DRIVER_MODULE_ARG[]   = WIFI_AP_DRIVER_MODULE_ARG;
+static const char AP_FIRMWARE_LOADER[]     = WIFI_AP_FIRMWARE_LOADER;
 static const char AP_DRIVER_PROP_NAME[]    = "wlan.ap.driver.status";
 
 #ifdef WIFI_EXT_MODULE_NAME
