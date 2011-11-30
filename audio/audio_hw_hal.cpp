10522bc4f5030388a1460e3eb11b4dd4d565b6e4
Ricardo Cerqueira <github@cerqueira.org>
2011-11-30 00:28:58 +0000

    audio_legacy: Convert channels to old enum
    Change-Id: Ica7dda26d7c416e2ac87d220b622bccee88ca74a

--- audio/audio_hw_hal.cpp
+++ audio/audio_hw_hal.cpp
@@ -81,7 +81,7 @@ static uint32_t out_get_channels(const struct audio_stream *stream)
 {
     const struct legacy_stream_out *out =
         reinterpret_cast<const struct legacy_stream_out *>(stream);
-    return out->legacy_out->channels();
+    return out->legacy_out->channels() >> 2;
 }
 
 static audio_format_t out_get_format(const struct audio_stream *stream)
@@ -452,6 +452,8 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
     out->legacy_out = ladev->hwif->openOutputStream(devices, format, channels,
                                                     sample_rate, &status);
 #endif
+
+    *channels = *channels >> 2;
 
     if (!out->legacy_out) {
         ret = status;
