43214e3ab149e0c033dc1d349cc1ad12a33243e9
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-05-05 03:17:02 -0700

    wifi.c: allow devices to specify driver delay
    This is necessary on the Samsung smdk4210 tablets. Without
    the delay, it takes roughly 30 seconds to enable wifi.
    
    Change-Id: I6a3419e66958fc72ae60e5294f52124b1180d752

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -106,8 +106,6 @@ struct genl_family *nl80211;
 #define WIFI_DRIVER_FW_PATH_PARAM	"/sys/module/wlan/parameters/fwpath"
 #endif
 
-#define WIFI_DRIVER_LOADER_DELAY	1000000
-
 static const char IFACE_DIR[]           = "/data/system/wpa_supplicant";
 #ifdef WIFI_DRIVER_MODULE_PATH
 static const char DRIVER_MODULE_NAME[]  = WIFI_DRIVER_MODULE_NAME;
@@ -291,7 +289,9 @@ int wifi_load_driver()
     }
 
     if (strcmp(FIRMWARE_LOADER,"") == 0) {
-        /* usleep(WIFI_DRIVER_LOADER_DELAY); */
+#ifdef WIFI_DRIVER_LOADER_DELAY
+        usleep(WIFI_DRIVER_LOADER_DELAY);
+#endif
         property_set(DRIVER_PROP_NAME, "ok");
     }
     else {
