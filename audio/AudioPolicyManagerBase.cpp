CyanogenMod/android_hardware_libhardware_legacy

commit 76e97d3950f2654adbb0a415218b6d048200c395

audio policy: force device update if disconnected
Force the new device selection to be applied when a device is disconnected
even if none is select. This is to indicate to the audio HAL that current
device selection is not valid anymore even if no new device is selected because
no audio track is active.

Bug 7141149.

Change-Id: Iade02fb53a4cc2051fce9da07870c5391396b696

audio/AudioPolicyManagerBase.cpp
@@ -169,7 +169,10 @@ status_t AudioPolicyManagerBase::setDeviceConnectionState(audio_devices_t device
 
         updateDevicesAndOutputs();
         for (size_t i = 0; i < mOutputs.size(); i++) {
-            setOutputDevice(mOutputs.keyAt(i), getNewDevice(mOutputs.keyAt(i), true /*fromCache*/));
+            setOutputDevice(mOutputs.keyAt(i),
+                            getNewDevice(mOutputs.keyAt(i), true /*fromCache*/),
+                            true,
+                            0);
         }
 
         if (device == AUDIO_DEVICE_OUT_WIRED_HEADSET) {

commit fe43d163d6646a175934f0752fa05e3e53e89da4

audio policy: fix volume problem - DO NOT MERGE
merge from master:

"audio policy: fix volume change when disabling SCO

commit 76e97d39 introduced a regression by having setDeviceConnectionState()
force a device change on duplicated outputs even if the selected device is 0.
This also forces a device 0 for the two outputs it is duplicated to,
which may override a valid device selection on those outputs and apply
default volumes.

Bug 7429869."

Change-Id: I37dd66343c54eb27e420089edbda04444fa1e2c6

audio/AudioPolicyManagerBase.cpp
@@ -169,9 +169,12 @@ status_t AudioPolicyManagerBase::setDeviceConnectionState(audio_devices_t device
 
         updateDevicesAndOutputs();
         for (size_t i = 0; i < mOutputs.size(); i++) {
+            // do not force device change on duplicated output because if device is 0, it will
+            // also force a device 0 for the two outputs it is duplicated to which may override
+            // a valid device selection on those outputs.
             setOutputDevice(mOutputs.keyAt(i),
                             getNewDevice(mOutputs.keyAt(i), true /*fromCache*/),
-                            true,
+                            !mOutputs.valueAt(i)->isDuplicated(),
                             0);
         }
 