60758e27a4be8fc9ac1180f8a4055234e1702cc9
Eric Laurent <elaurent@google.com>
2013-01-17 17:27:26 -0800

    audio policy: added front + back mic
    Added definition to parse front + back mic from input descriptors
    in audio_policy.conf file.
    
    Change-Id: I9ebdeac664e12104589a52d15acbbfc4db787923

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -3406,6 +3406,7 @@ const struct StringToEnum sOutChannelsNameToEnumTable[] = {
 const struct StringToEnum sInChannelsNameToEnumTable[] = {
     STRING_TO_ENUM(AUDIO_CHANNEL_IN_MONO),
     STRING_TO_ENUM(AUDIO_CHANNEL_IN_STEREO),
+    STRING_TO_ENUM(AUDIO_CHANNEL_IN_FRONT_BACK),
 };
 
 
