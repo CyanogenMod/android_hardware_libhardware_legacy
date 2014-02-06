4387d30660db3c9a10b16b3a4a1c986c330e788b
Patrick Tjin <pattjin@google.com>
2014-02-05 16:03:30 -0800

    audio_policy: clean up dumpsys
    Remove NUL characters from audio_policy's dump output
    
    Change-Id: If591473dbe3e5adbc34dbbdb0d552b329f19eb6a

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -3521,7 +3521,7 @@ void AudioPolicyManagerBase::HwModule::dump(int fd)
     result.append(buffer);
     write(fd, result.string(), result.size());
     if (mOutputProfiles.size()) {
-        write(fd, "  - outputs:\n", sizeof("  - outputs:\n"));
+        write(fd, "  - outputs:\n", strlen("  - outputs:\n"));
         for (size_t i = 0; i < mOutputProfiles.size(); i++) {
             snprintf(buffer, SIZE, "    output %d:\n", i);
             write(fd, buffer, strlen(buffer));
@@ -3529,7 +3529,7 @@ void AudioPolicyManagerBase::HwModule::dump(int fd)
         }
     }
     if (mInputProfiles.size()) {
-        write(fd, "  - inputs:\n", sizeof("  - inputs:\n"));
+        write(fd, "  - inputs:\n", strlen("  - inputs:\n"));
         for (size_t i = 0; i < mInputProfiles.size(); i++) {
             snprintf(buffer, SIZE, "    input %d:\n", i);
             write(fd, buffer, strlen(buffer));
