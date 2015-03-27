3ca773310ade547e540e878d748cd10e8e9f1282
Ethan Chen <intervigil@gmail.com>
2015-03-26 22:16:46 -0700

    audio: Make getPresentationPosition pure virtual
    * Allows compilation of libaudiohw_legacy static library
    
    Change-Id: I84c9ccb6cb3a6c744f83380cfe8c33ad5142eacf

--- include/hardware_legacy/AudioHardwareInterface.h
+++ include/hardware_legacy/AudioHardwareInterface.h
@@ -134,7 +134,7 @@ public:
     /**
      * Return a recent count of the number of audio frames presented to an external observer.
      */
-    virtual status_t    getPresentationPosition(uint64_t *frames, struct timespec *timestamp);
+    virtual status_t    getPresentationPosition(uint64_t *frames, struct timespec *timestamp) = 0;
 
 };
 
